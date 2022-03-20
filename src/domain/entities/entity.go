8c19113f318b3cb22191cbbf54bc532515b2f772
elioenaiferrari <elioenaiferrari@gmail.com>
2022-03-20 14:46:43 -0300

    register student

--- src/domain/entities/entity.go
+++ src/domain/entities/entity.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Entity struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primarykey"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
