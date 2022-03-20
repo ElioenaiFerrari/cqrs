b2333d7277962f35112b5d18a13af8df797056f8
elioenaiferrari <elioenaiferrari@gmail.com>
2022-03-20 11:46:15 -0300

    register student

--- src/presentation/controllers/students.go
+++ src/presentation/controllers/students.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/ElioenaiFerrari/cqrs/src/app/commands"
 	"github.com/ElioenaiFerrari/cqrs/src/domain/entities"
 	"github.com/labstack/echo/v4"
@@ -20,12 +22,12 @@ func (sc *StudentsController) Create(c echo.Context) error {
 	var student entities.Student
 
 	if err := c.Bind(&student); err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	if err := sc.registerStudentCommand.Execute(&student); err != nil {
-		return err
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	return c.JSON(201, student)
+	return c.JSON(http.StatusCreated, student)
 }
